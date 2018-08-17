b342312b568e15ea36b4248d956f97130d2e1eb9
Lorenzo Corral <lcorral9@gmail.com>
2018-08-17 15:27:34 -0500

    Adding two projects that i've completed

---
+++ I Am Rich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Lorenzo Corral on 5/22/18.
+//  Copyright © 2018 Lorenzo Corral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
